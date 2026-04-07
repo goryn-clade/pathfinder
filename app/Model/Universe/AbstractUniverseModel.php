ea31054d0cc5a8b9ea8b9f7dd50f2e81c7d5305b
Sam ONeill <samuel.p.oneill@gmail.com>
2026-04-08 10:01:19 +1200

    Fix parameter type declarations in AbstractUniverseModel (Universe)
    - set_position(array<string, mixed> $position)
    - beforeUpdateEvent(self, array $pkeys)
    - loadById(int, string, array<string, mixed>)
    - abstract loadData(int, string, array<string, mixed>)
    - buildTableIndex(AbstractUniverseModel, array<string>)
    - generateHashKeyRow(string, string|int)

--- app/Model/Universe/AbstractUniverseModel.php
+++ app/Model/Universe/AbstractUniverseModel.php
@@ -41,7 +41,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param $position
      * @return null
      */
-    public function set_position($position){
+    public function set_position(array<string, mixed> $position){
         $position = (array)$position;
         if(count($position) === 3){
             $this->x = $position['x'];
@@ -58,7 +58,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param $pkeys
      * @return bool
      */
-    public function beforeUpdateEvent($self, $pkeys) : bool {
+    public function beforeUpdateEvent(self $self, array $pkeys) : bool {
         // if model changed, 'update' col needs to be updated as well
         // -> data no longer "outdated"
         $self->touch('updated');
@@ -148,7 +148,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param string $accessToken
      * @param array $additionalOptions
      */
-    public function loadById(int $id, string $accessToken = '', array $additionalOptions = []){
+    public function loadById(int $id, string $accessToken = '', array<string, mixed> $additionalOptions = []){
         /**
          * @var self $model
          */
@@ -164,7 +164,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param string $accessToken
      * @param array $additionalOptions
      */
-    abstract protected function loadData(int $id, string $accessToken = '', array $additionalOptions = []);
+    abstract protected function loadData(int $id, string $accessToken = '', array<string, mixed> $additionalOptions = []);
 
     /**
      * convert CCPs ids for system security into Pathfinder security label
@@ -197,7 +197,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param AbstractUniverseModel $model
      * @param array $rowKeys
      */
-    public static function buildTableIndex(AbstractUniverseModel $model, array $rowKeys = []){
+    public static function buildTableIndex(AbstractUniverseModel $model, array<string> $rowKeys = []){
         $hashKeyTable = static::generateHashKeyTable($model->getTable());
         if( !self::getF3()->exists($hashKeyTable, $cachedData) ){
             $cachedData = [];
@@ -213,7 +213,7 @@ abstract class AbstractUniverseModel extends AbstractModel {
      * @param $value
      * @return string
      */
-    public static function generateHashKeyRow(string $table, $value) : string {
+    public static function generateHashKeyRow(string $table, string|int $value) : string {
         return static::generateHashKeyTable($table) . '_' .  md5(strtolower((string)$value));
     }
 
