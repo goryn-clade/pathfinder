50476aebf1426b179c2462dbe4249289885c0872
Sam ONeill <samuel.p.oneill@gmail.com>
2026-04-08 15:48:13 +1200

    Fix parameter type declarations in Log REST controller
    - Log.php: Add value type to update method logData parameter (array<string, mixed>)
    
    This addresses parameter type PHPStan errors.

--- app/Controller/Api/Rest/Log.php
+++ app/Controller/Api/Rest/Log.php
@@ -92,7 +92,7 @@ class Log extends AbstractRestController {
      * @return bool|Pathfinder\ConnectionLogModel
      * @throws \Exception
      */
-    private function update(int $logId, array $logData){
+    private function update(int $logId, array<string, mixed> $logData){
         $log = false;
         if($logId){
             $activeCharacter = $this->getCharacter();
