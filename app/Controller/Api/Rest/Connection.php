aeaadfca662a78d16292b3edd2d23d1846f5532c
Sam ONeill <samuel.p.oneill@gmail.com>
2026-04-08 20:06:51 +1200

    Add void return types to REST API controller methods
    - Connection: get, put, delete
    - System: get, put, patch, delete
    - Map: put, patch, delete

--- app/Controller/Api/Rest/Connection.php
+++ app/Controller/Api/Rest/Connection.php
@@ -18,7 +18,7 @@ class Connection extends AbstractRestController {
      * @param $params
      * @throws \Exception
      */
-    public function get(\Base $f3, array<string, mixed> $params){
+    public function get(\Base $f3, array<string, mixed> $params) : void {
         $requestData = $this->getRequestData($f3);
         $connectionIds = array_map('intval', explode(',', (string)$params['id']));
         $addData = (array)$requestData['addData'];
@@ -64,7 +64,7 @@ class Connection extends AbstractRestController {
      * @param \Base $f3
      * @throws \Exception
      */
-    public function put(\Base $f3){
+    public function put(\Base $f3) : void {
         $requestData = $this->getRequestData($f3);
         $connectionData = [];
 
@@ -122,7 +122,7 @@ class Connection extends AbstractRestController {
      * @param $params
      * @throws \Exception
      */
-    public function delete(\Base $f3, array<string, mixed> $params){
+    public function delete(\Base $f3, array<string, mixed> $params) : void {
         $requestData = $this->getRequestData($f3);
         $connectionIds = array_map('intval', explode(',', (string)$params['id']));
         $deletedConnectionIds = [];
--- app/Controller/Api/Rest/Map.php
+++ app/Controller/Api/Rest/Map.php
@@ -24,7 +24,7 @@ class Map extends AbstractRestController {
      * @param       $test
      * @throws \Exception
      */
-    public function put(\Base $f3, array<string, mixed> $test){
+    public function put(\Base $f3, array<string, mixed> $test) : void {
         $requestData = $this->getRequestData($f3);
 
         /**
@@ -41,7 +41,7 @@ class Map extends AbstractRestController {
      * @param       $params
      * @throws \Exception
      */
-    public function patch(\Base $f3, array<string, mixed> $params){
+    public function patch(\Base $f3, array<string, mixed> $params) : void {
         $requestData = $this->getRequestData($f3);
         $mapData = [];
 
@@ -66,7 +66,7 @@ class Map extends AbstractRestController {
      * @param       $params
      * @throws \Exception
      */
-    public function delete(\Base $f3, array<string, mixed> $params){
+    public function delete(\Base $f3, array<string, mixed> $params) : void {
         $deletedMapIds = [];
 
         if($mapId = (int)$params['id']){
