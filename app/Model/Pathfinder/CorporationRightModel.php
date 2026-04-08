d1bc406dd2bfa6adc10bfd444a899b0c01ee838d
Sam ONeill <samuel.p.oneill@gmail.com>
2026-04-08 16:25:58 +1200

    Fix parameter type declaration in CorporationRightModel
    - setData: array<string, mixed> $data

--- app/Model/Pathfinder/CorporationRightModel.php
+++ app/Model/Pathfinder/CorporationRightModel.php
@@ -66,7 +66,7 @@ class CorporationRightModel extends AbstractPathfinderModel {
      * set data by associative array
      * @param array $data
      */
-    public function setData($data){
+    public function setData(array<string, mixed> $data){
         unset($data['id']);
         unset($data['created']);
         unset($data['updated']);
